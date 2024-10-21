c094e81e424a9dba1fc0fdbf3c74f78b104a2c2e
galkin1928 <galkin1928@gmail.com>
2024-10-21 20:10:16 +0300

    Initial Commit

---
+++ GalkinHomework2/GalkinHomework2App.swift
@@ -0,0 +1,17 @@
+//
+//  GalkinHomework2App.swift
+//  GalkinHomework2
+//
+//  Created by GALKIN Aleksandr on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GalkinHomework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
